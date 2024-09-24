f60c58300b9276da4448be3638969210bf515d1f
Maxime Castres <mcastres@student.42.fr>
2024-09-24 14:58:36 +0200

    Update: function

--- next/lib/strapi/strapiImage.ts
+++ next/lib/strapi/strapiImage.ts
@@ -1,5 +1,13 @@
+import { unstable_noStore as noStore } from 'next/cache';
+
 export function strapiImage(url: string): string {
+  noStore();
   if (url.startsWith("/")) {
+
+    if (!process.env.NEXT_PUBLIC_API_URL && document.location) {
+      return `https://${document.location.host.replace("client-", "api-")}${url}`
+    }
+
     return process.env.NEXT_PUBLIC_API_URL + url
   }
   return url
