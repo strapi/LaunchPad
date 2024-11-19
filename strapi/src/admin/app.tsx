a43eaa41da7fba32f94793b54c64d14df6a13501
Rémi de Juvigny <remi.dejuvigny@strapi.io>
2024-11-19 09:32:46 +0100

    chore: delete cache revalidation panel

--- strapi/src/admin/app.tsx
+++ strapi/src/admin/app.tsx
@@ -1,6 +1,5 @@
 import type { StrapiApp } from "@strapi/strapi/admin";
 
-import PreviewButton from "./extensions/components/PreviewButton";
 import { unstable_useContentManagerContext as useContentManagerContext } from "@strapi/strapi/admin";
 
 export default {
@@ -35,27 +34,5 @@ export default {
     ],
   },
 
-  bootstrap(app: StrapiApp) {
-    // @ts-ignore
-    // app.getPlugin("content-manager").apis.addEditViewSidePanel([
-    //   () => {
-    //     const cmCtx = useContentManagerContext();
-    //     const modelsWithoutPreview = [
-    //       "api::category.category",
-    //       "api::logo.logo",
-    //       "api::faq.faq",
-    //       "api::plan.plan",
-    //       "api::redirection.redirection",
-    //       "api::testimonial.testimonial",
-    //     ];
-    //     if (modelsWithoutPreview.includes(cmCtx.model)) {
-    //       return null;
-    //     }
-    //     return {
-    //       title: "Cache",
-    //       content: <PreviewButton />,
-    //     };
-    //   },
-    // ]);
-  },
+  bootstrap(app: StrapiApp) {},
 };
