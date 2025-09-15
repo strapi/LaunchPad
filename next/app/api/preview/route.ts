e35411058ab3c600a4a41bd614a0db18a905ad0d
Rémi de Juvigny <8087692+remidej@users.noreply.github.com>
2025-09-15 12:22:28 -0400

    feat: setup live preview inline editing (#69)
    * feat: setup preview inline editing
    
    (cherry picked from commit f611284285f8376c3ff2a42d449c3b0ad1e9ad83)
    
    * chore: more preview url building to strapi config
    
    (cherry picked from commit 0015f26140817af18c76762c4fced063ee145001)
    
    * fix: only encode source maps in draft mode
    
    (cherry picked from commit d58c5b2b9cedbed56454bcc4bc6103c5a0a8e18e)

--- next/app/api/preview/route.ts
+++ next/app/api/preview/route.ts
@@ -4,37 +4,24 @@ import { redirect } from 'next/navigation';
 export const GET = async (request: Request) => {
   const { searchParams } = new URL(request.url);
   const secret = searchParams.get('secret');
-  const slug = searchParams.get('slug');
-  const locale = searchParams.get('locale');
-  const uid = searchParams.get('uid');
+  const url = searchParams.get('url') ?? '/';
   const status = searchParams.get('status');
 
+  // Check the secret and next parameters
+  // This secret should only be known to this route handler and the CMS
   if (secret !== process.env.PREVIEW_SECRET) {
     return new Response('Invalid token', { status: 401 });
   }
 
-  const contentType = uid?.split('.').pop();
-
-  // Specific for the application
-  let slugToReturn = `/${locale}/${contentType}`;
-
-  if (contentType === 'page' || contentType === 'global') {
-    if (slug && slug !== 'homepage') {
-      slugToReturn = `/${locale}/${slug}`;
-    } else {
-      slugToReturn = `/${locale}`;
-    }
-  } else if (contentType === 'article' || contentType?.includes('blog')) {
-    slugToReturn = `/${locale}/blog${slug ? `/${slug}` : ''}`;
-  } else if (contentType?.includes('product')) {
-    slugToReturn = `/en/products${slug ? `/${slug}` : ''}`;
-  }
-
   const draft = await draftMode();
-  if (status === 'draft') {
-    draft.enable();
-  } else {
+
+  if (status === 'published') {
+    // Make sure draft mode is disabled so we only query published content
     draft.disable();
+  } else {
+    // Enable draft mode so we can query draft content
+    draft.enable();
   }
-  redirect(slugToReturn);
+
+  redirect(url);
 };
